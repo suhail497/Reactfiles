5caaf4fe6140f0c453d364c2203e4be4337b62e2
suhail497 <asuhail235@gmail.com>
2020-03-04 10:04:52 +0530

    1. JSX, 2. Material UI, 3. Components Started

---
+++ src/1. JSXelemets.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+const NestedElements = () =>{
+
+    return(
+        <div>
+            <h1> General JSX</h1>
+            <p> 
+            At W3Schools you will find complete references about tags, attributes, events, color names, entities, character-sets, URL encoding, language codes, HTTP messages, and more.
+            </p>
+        </div>
+    )
+}
+
+const Attribute = ()=>{
+
+    return(
+        <div>
+        <h1>Header</h1>
+            <h2>Content</h2>
+            <p data-myattribute = "somevalue">This is the content!!!</p>
+        </div>
+    )
+}
+
+
+// Javascript Expression
+const Expression =()=>{
+
+    var a = 10
+    var b = 11
+    var c,i = 0
+        return(
+            <div>
+            {/* 1. General Expression with Data Binding*/}
+             <h1>{c = a+b}</h1>
+             <h2>{c}</h2> 
+
+             {/* 2. Ternary Operator  */}
+             <h1>{  i === 1 ? 'True!' : 'False'}</h1>
+
+
+            </div>
+        )
+}
+
+// 4.General Styling
+   const Styling=()=>{
+    var myStyle = {
+        fontSize: 100,
+        color: '#FF0000'
+    }
+            return(
+                    <div>
+                            {/* style  => Mapping Attribute/Function = {} => Data Binding {} => Object with Key Value Pairs */}
+                        <h1 style = { {color:'blue',fontSize:50} }> Heading Tag with Color</h1>
+                        <h2 style= {myStyle} >Var MyStyle Object</h2>
+                    </div>
+
+            )
+
+   } 
+
+
+export {NestedElements, Attribute, Expression,Styling}
+
+// Naming Convention
+// HTML tags always use lowercase tag names, while React components start with Uppercase.
+// Note − You should use className and htmlFor as XML attribute names instead of class and for.
+// This is explained on React official page as −
+// Since JSX is JavaScript, identifiers such as class and for are discouraged as XML attribute names. 
+// Instead, React DOM components expect DOM property names such as className and htmlFor, respectively.
+
+
--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// React Import
+import React from "react";
 
-function App() {
+// 1. JSX Imports
+import {NestedElements, Attribute, Expression,Styling} from './1. JSXelemets'
+
+// 2. Components Imports
+import {Stateless} from './2. Components'
+
+// Material UI Imports
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import RestoreIcon from "@material-ui/icons/Restore";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+
+// makeStyles function from Material UI
+const useStyles = makeStyles({ 
+  root: {
+    width: 500
+  },
+  label: {
+    color: "red"
+  }
+});
+
+export default function App() {
+  const classes = useStyles();
+
+  const [value, setValue] = React.useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Suhail <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    
+    <div>
+
+        {/* 1. JSX [JavaScript Extended] */}
+
+              {/* 1.1 Nested Elements */}
+              <NestedElements />
+          
+              {/* 1.2 Attributes */}
+              <Attribute />
+
+              {/* 1.3 Expression */}
+              <Expression />
+
+              {/* 1.4 Styling */}
+              <Styling/>
+
+        {/* 2. Components  */}
+
+            {/* 2.1 Stateless Component */}
+            <Stateless />
+
+            
+        {/* Material UI */}
+        
+        <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        showLabels
+        className={classes.root}
+      >
+        <BottomNavigationAction
+          className={classes.root}
+          label="Recents"
+          icon={<RestoreIcon />}
+        />
+        <BottomNavigationAction
+          className={classes.label}
+          label="Favorites"
+          icon={<FavoriteIcon />}
+        />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+
     </div>
+    
   );
 }
-
-export default App;
+// 
